61861f38c573e5abef347fcb54ab8964fc4b5052
wangfei0904306 <308051938@qq.com>
2017-05-15 22:12:09 +0800

    qqad

--- app/src/main/java/com/waskj/hongloumeng/qqad/SplashActivity.java
+++ app/src/main/java/com/waskj/hongloumeng/qqad/SplashActivity.java
@@ -172,7 +172,7 @@ public class SplashActivity extends Activity implements SplashADListener {
     public void onNoAD(int errorCode) {
         Log.i("AD_DEMO", "LoadSplashADFail, eCode=" + errorCode);
         /** 如果加载广告失败，则直接跳转 */
-        this.startActivity(new Intent(this, MainActivity.class));
+        //this.startActivity(new Intent(this, MainActivity.class));
         this.finish();
     }
 
@@ -182,7 +182,7 @@ public class SplashActivity extends Activity implements SplashADListener {
      */
     private void next() {
         if (canJump) {
-            this.startActivity(new Intent(this, MainActivity.class));
+            //this.startActivity(new Intent(this, MainActivity.class));
             this.finish();
         } else {
             canJump = true;
